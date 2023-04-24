956d69fdff0267f9eb0d872c1b4e896f74a53c8d
Averines <averines@gmail.com>
2023-04-24 16:27:12 +0300

    add: options css

--- options/options.js
+++ options/options.js
@@ -1,20 +1,32 @@
+// получаем сохраненные настройки пользователя
+const userOptions = await chrome.storage.sync.get("options");
+console.log(userOptions);
+
+const bodyEl = document.querySelector("body");
+if (userOptions.options.theme) {
+    bodyEl.classList.add(userOptions.options.theme)
+}
+
+
+// определяем опции
 const options = {};
 const optionsForm = document.getElementById("optionsForm");
 
-console.log(optionsForm);
-
-optionsForm.tabcompact.addEventListener("change", (e) => {
-    options.tabcompact = e.target.checked;
-    chrome.storage.sync.set({ options });
-});
-
-
-optionsForm.theme.forEach(r =>
-    r.addEventListener("change", (e) => {
-        options.theme = e.target.value;
+if (optionsForm.theme) {
+    optionsForm.tabcompact.addEventListener("change", (e) => {
+        options.tabcompact = e.target.checked;
         chrome.storage.sync.set({ options });
-        console.log(options);
-    }));
+    });
+}
+
+if (optionsForm.theme) {
+    optionsForm.theme.forEach(r =>
+        r.addEventListener("change", (e) => {
+            options.theme = e.target.value;
+            chrome.storage.sync.set({ options });
+            console.log(options);
+        }));
+}
 
 
 const data = await chrome.storage.sync.get("options");
